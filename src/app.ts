3bb7e408b77795c03f716aa57ab33954f67a91ef
Laxmi Shrestha <shresthalaxmi0825@gmail.com>
2024-12-12 21:09:10 +0545

    database setup

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,28 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
+import { Sequelize } from 'sequelize';
+import db from './models/index.js';
 const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('helloooooooooo');
+  res.send('hello laxmi');
 });
+
+async function connectToDatabase() {
+  try {
+    await db.sequelize.authenticate();
+    // tslint:disable-next-line:no-console
+    console.log('success!');
+  } catch (error) {
+    // tslint:disable-next-line:no-console
+    console.log('failure: ', error);
+  }
+}
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`Server active at http://localhost:${port}`);
+  connectToDatabase();
 });
